9a00aaca059a78644217d430a72497ed03121444
Gustavo Neves De Faria <142701488+GustavoNevesDeFaria@users.noreply.github.com>
2024-10-14 22:36:37 -0300

    Add files via upload

---
+++ MVC/Resources/View/Pages/admin/java/java.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.sub-list').click(function () {
+        $(this).next('.sub-menu').slideToggle();
+        $(this).find('.dropdown').toggleClass('rotate');
+    });
+    $('.menu-btn').click(function () {
+        $('.side-bar').addClass('active');
+        $('.menu-btn').css("visibility", "hidden");
+    });
+    $('.close-btn').click(function () {
+        $('.side-bar').removeClass('active');
+        $('.menu-btn').css("visibility", "visible");
+    });
+});
