5fe5e7611a6167eb44104723541b67c2d790a5f5
d4n13lx <daniel.kazimoto83@gmail.com>
2024-11-28 10:08:32 -0300

    Atualizando o index

--- index.php
+++ index.php
@@ -3,14 +3,17 @@
 require __DIR__.'/includes/app.php';
 use MVC\App\HTTP\Router;
 
-//Inicia o router
+// Inicia o router
 $obrouter = new Router(URL);
 
-//Inclui as rotas das Paginas 
-@include __DIR__.'/MVC/routes/Pages.php';
+// Inclui as rotas das Paginas 
+include __DIR__.'/MVC/routes/Pages.php';
 
-//inclui a rota do painel
-@include __DIR__.'/MVC/routes/admin.php';
+// Inclui a rota do painel
+include __DIR__.'/MVC/routes/admin.php';
+
+// Inclui as rotas da API
+include __DIR__.'/MVC/routes/api.php';
 
 //Imprime o Response da Rota 
 $response = $obrouter->run();
